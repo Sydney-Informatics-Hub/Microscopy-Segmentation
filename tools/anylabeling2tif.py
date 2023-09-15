15cd7332885f62f93db6b210e6e6e8ba46a09e43
Seb Haan <sebhaan@gmail.com>
2023-09-15 11:10:27 +1000

    Add anylabeling to tif mask converter

---
+++ tools/anylabeling2tif.py
@@ -0,0 +1,103 @@
+"""
+Converts Anylabeling json annotation files to multistack tiff images.
+"""
+
+import os
+import pandas as pd
+import json
+import numpy as np
+import rasterio
+from rasterio.features import geometry_mask
+from shapely.geometry import Polygon, MultiPolygon
+
+import cv2
+import tifffile as tiff
+
+
+def anylabeling2df(path_json, outfname = None, get_z = False):
+    """
+    Write annotations in anylabeling json files to pandas dataframe.
+
+    Parameters
+    ----------
+    path_json : str
+        Path to json files. Json files must be named as follows: 'image_z<z>.json' or image_<z>.json where <z> is the z-coordinate of the image.
+    outfname : str, optional
+        Path + filename for output file in imod .mod format. If None, file is written to same path as json files.
+    get_z : bool
+        If True, z-coordinate is extracted from filename. If False, no z-coordinate is extracted.
+
+    Returns
+    -------
+    df : pandas dataframe
+    json_files : list
+    """
+    # get all json files
+    json_files = [f for f in os.listdir(path_json)   if f.endswith('.json')]
+
+    if get_z:
+        # sort files by z-coordinate
+        json_files = sorted(json_files, key = lambda x: int(x.split('_')[1].split('.')[0].replace('z', '')))
+        # extract z-coordinate from filename as integer
+        z_list = [int(f.split('_')[1].split('.')[0].replace('z', '')) for f in json_files]
+
+    # define dataframe to save all points
+    df_all = pd.DataFrame(columns = ['label', 'points', 'shapetype', 'imagePath', 'imageHeight', 'imageWidth', 'z'])
+    #image_list = []
+    for i, f in enumerate(json_files):
+        # read in json files
+        with open(os.path.join(path_json, f), 'r') as f:
+            data = json.load(f)
+        # get image name
+        # image_list.append(data["imagePath"])
+        # get z-coordinate
+        if get_z:
+            z = z_list[i]
+        else:
+            z = None
+        # get shapes
+        shapes = data['shapes']
+        # loop over shapes
+        for j, shape in enumerate(shapes):
+            label = shape['label']
+            # get points
+            points = shape['points']
+            # create dataframe
+            df = pd.DataFrame(columns = ['label', 'points', 'shapetype', 'imagePath', 'imageHeight', 'imageWidth', 'z'])
+            df['points'] = [points]#[[item for sublist in points for item in sublist]]
+            df['label'] = label
+            df['shapetype'] = shape['shape_type']
+            df['imagePath'] = data['imagePath']
+            df['imageHeight'] = data['imageHeight']
+            df['imageWidth'] = data['imageWidth']
+            df['z'] = z
+            # combine with df_all
+            df_all = pd.concat([df_all, df], ignore_index = True)
+
+        # Save points as txt file
+    df_all.to_csv(outfname, index = False, header = False)
+    return df_all, json_files
+
+
+
+
+
+def write_polygons_to_tiff(df):
+    # Generate a unique color for each label. 
+    unique_labels = df['label'].unique()
+    color_map = {label: (i+1)*10 for i, label in enumerate(unique_labels)}  # Assigning a unique grayscale value for simplicity
+
+    for imagePath, sub_df in df.groupby('imagePath'):
+        # Create a blank image
+        image_data = np.zeros((int(sub_df['imageHeight'].values[0]), int(sub_df['imageWidth'].values[0])), dtype=np.uint8)
+
+        # For each polygon in the sub-DataFrame, draw the polygon onto the blank image using OpenCV
+        for idx, row in sub_df.iterrows():
+            pts = np.array(row['points'], np.int32)
+            pts = pts.reshape((-1, 1, 2))
+            cv2.fillPoly(image_data, [pts], color_map[row['label']])
+        
+        # Save the image as TIFF using tifffile
+        output_path = imagePath.replace('.tif', '_mask.tif')
+        tiff.imwrite(output_path, image_data)
+
