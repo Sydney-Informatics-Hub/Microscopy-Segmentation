227c73ae845e57716635a28fcca94a4b4183a5f7
Seb Haan <sebhaan@gmail.com>
2023-11-06 14:44:51 +1100

    Implement export to multiple mesh formats and marching cube

--- tools/tiff2mesh.py
+++ tools/tiff2mesh.py
@@ -1,11 +1,29 @@
-# sconverting array to point cloud and saving as mesh
+"""
+ Converting tif image array stack to point cloud and saving as mesh
+ Supports multiple mesh export formats incl VRML, OBJ, STL, PLY, VTK
+
+    Usage:
+    python tiff2mesh.py --directory data --filename_pattern {}.tif --filename_out cellmesh_test.wrl --file_format vrml --grid_spacing 1 1 1
+
+    Arguments:
+    --directory: directory where tif images are located
+    --filename_pattern: filename pattern for tif images
+    --filename_out: filename for output mesh
+    --file_format: file format for output mesh
+    --grid_spacing: grid spacing for output mesh
+    
+"""
+
 
 import numpy as np
 import pyvista as pv
 import meshio
 import argparse
+import os
+from PIL import Image
+import vtk
 
-def arrays_to_point_cloud(arrays):
+def arrays_to_point_cloud(arrays, spacing = [1,1,1]):
     """
     Convert a stack of 2D binary numpy arrays into a 3D point cloud.
 
@@ -13,6 +31,8 @@ def arrays_to_point_cloud(arrays):
     :return: A numpy array of points.
     """
     points = np.argwhere(arrays)  # Get the indices of points where the array is not zero
+    # multiply with spacing in 3D 
+    points *= spacing
     return points
 
 def point_cloud_to_mesh(points):
@@ -33,19 +53,100 @@ def save_mesh(mesh, filename, file_format = 'obj'):
 
     :param mesh: A PyVista mesh object.
     :param filename: The output file path.
+    :param file_format: The file format to save as (e.g., "stl", "ply", "obj", "vrml").
     """
-    cells = [("triangle", mesh.faces.reshape(-1, 4)[:, 1:])]
-    meshio.write_points_cells(
-        filename,
-        mesh.points,
-        cells,
-        file_format=file_format
-    )
 
-def main(array_stack, filename, file_format="obj"):
-    points = arrays_to_point_cloud(array_stack)
-    surface = point_cloud_to_mesh(points)
-    save_mesh(surface, filename, file_format)
+    if file_format.lower() == 'vrml':
+        plotter = pv.Plotter(off_screen=True)
+        plotter.add_mesh(mesh)
+        plotter.show(auto_close=False) 
+        vrml_exporter = vtk.vtkVRMLExporter()
+        vrml_exporter.SetFileName(filename)
+        vrml_exporter.SetRenderWindow(plotter.ren_win)
+        vrml_exporter.Write()
+        #vrml_exporter.SetInputData(mesh)
+        #vrml_exporter.Write()
+        plotter.close()
+    else:
+        cells = [("triangle", mesh.faces.reshape(-1, 4)[:, 1:])]
+        meshio.write_points_cells(
+            filename,
+            mesh.points,
+            cells,
+            file_format=file_format
+        )
+
+def numpy_stack_to_mesh(array_stack, filename, threshold = 0.9, file_format="obj", grid_spacing = (1, 1, 1)):
+    """
+    Convert a 3D NumPy array stack to a mesh using PyVista's marching cubes algorithm.
+
+    :param array_stack: A 3D numpy array stack.
+    :param filename: The filename to save the mesh.
+    :param threshold: The threshold level to apply the marching cubes algorithm.
+    :param file_format: Format to save the mesh in (e.g., "stl", "ply", "obj", "vtk").
+    :param grid_spacing: The grid spacing in each dimension. Defaults to (1, 1, 1).
+    """
+    # Create a PyVista grid from the NumPy array stack
+    grid = pv.UniformGrid()
+    grid.dimensions = np.array(array_stack.shape)
+    # For a UniformGrid, the "spacing" attribute defines the distance between adjacent points on the grid
+    # If your data is isotropic (equal spacing in all directions), it can be left as the default (1.0, 1.0, 1.0)
+    # If you have different spacing, you would set it here
+    grid.spacing = grid_spacing  # (optional) Adjust this if your data has non-uniform spacing
+    
+    # Flatten the array_stack and assign it to the grid
+    grid.point_data["values"] = array_stack.flatten(order="F")  # Use Fortran order to match PyVista's indexing
+    
+    # Run the marching cubes algorithm
+    contours = grid.contour([threshold])
+
+    # Save the mesh to a file
+    save_mesh(contours, filename, file_format)
+
+
+
+def tif_images_to_numpy_array_stack(directory, filename_pattern, image_count=None):
+    """
+    Convert a stack of TIF images into a NumPy array stack.
+
+    :param directory: Directory where the TIF images are located.
+    :param filename_pattern: A common filename pattern where an integer is replaced by '{}'.
+    :param image_count: The number of images to load.
+    :return: A NumPy array stack of the images.
+    """
+    # Initialize a list to hold the image data
+    images = []
+
+    if image_count is None:
+        # list all files with the given pattern in directory
+        filename_list = [f for f in os.listdir(directory) if filename_pattern.format(0) in f]
+        image_count = len(filename_list)
+
+    # Loop over the image numbers and load each image
+    for i in range(image_count):
+        # Construct the full filename for each image
+        filename = os.path.join(directory, filename_pattern.format(i))
+        
+        # Open the image and convert to a NumPy array
+        image = Image.open(filename)
+        image_array = np.array(image)
+        
+        # Append the image data to the list
+        images.append(image_array)
+    
+    # Stack the individual image arrays into a single 3D NumPy array
+    return np.stack(images, axis=-1)
+
+
+def main(directory, filename_pattern, filename_out, file_format="obj", grid_spacing = (1, 1, 1)):
+    array_stack = tif_images_to_numpy_array_stack(directory, filename_pattern)
+    #points = arrays_to_point_cloud(array_stack)
+    #surface = point_cloud_to_mesh(points)   
+    #save_mesh(surface, filename, file_format)
+    numpy_stack_to_mesh(array_stack, filename_out, threshold = 0.9, file_format=file_format, grid_spacing=grid_spacing)
+
+
+### test function
 
 # Test function to create synthetic data and test the pipeline
 def test_conversion():
@@ -60,21 +161,35 @@ def test_conversion():
     surface = point_cloud_to_mesh(points)
     
     # Save mesh as VRML
-    vrml_filename = 'synthetic_data_mesh.obj'
-    save_mesh(surface, vrml_filename, file_format='obj')
+    mesh_filename = 'synthetic_data_mesh.obj'
+    save_mesh(surface, mesh_filename, file_format='obj')
 
-    return vrml_filename
+    return mesh_filename
 
-def main(array_stack, filename, file_format="vrml"):
-    points = arrays_to_point_cloud(array_stack)
-    surface = point_cloud_to_mesh(points)
-    save_mesh(surface, filename, file_format)
+def test_tif_images_to_numpy_array_stack():
+    directory = 'data'
+    filename_pattern = '{}.tif'
+    image_count = 5
+    array_stack = tif_images_to_numpy_array_stack(directory, filename_pattern, image_count)
 
-# write main execution with argparse
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Convert a stack of 2D binary numpy arrays to a 3D mesh.')
-    parser.add_argument('array_stack', type=str, help='The path to the input stack of 2D binary numpy arrays.')
-    parser.add_argument('filename', type=str, help='The name of the output mesh file.')
-    parser.add_argument('--file_format', type=str, default='obj', help='The file format of the output mesh file.')
+
+
+def test_main():
+    directory = 'data'
+    filename_pattern = '{}.tif'
+    #filename_out = 'cellmesh_test.obj'
+    filename_out = 'cellmesh_test.wrl'
+    file_format="vrml"
+    grid_spacing = (1, 1, 1)
+    main(directory, filename_pattern, filename_out, file_format=file_format, grid_spacing = grid_spacing)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Convert tif images to mesh')
+    parser.add_argument('--directory', type=str, default='data', help='directory where tif images are located')
+    parser.add_argument('--filename_pattern', type=str, default='{}.tif', help='filename pattern for tif images')
+    parser.add_argument('--filename_out', type=str, default='cellmesh_test.wrl', help='filename for output mesh')
+    parser.add_argument('--file_format', type=str, default='vrml', help='file format for output mesh')
+    parser.add_argument('--grid_spacing', type=float, nargs=3, default=[1,1,1], help='grid spacing for output mesh')
     args = parser.parse_args()
-    main(args.array_stack, args.filename, args.file_format)
+    main(args.directory, args.filename_pattern, args.filename_out, file_format=args.file_format, grid_spacing = args.grid_spacing)
